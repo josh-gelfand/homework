ac5c582efb3d16139be8f194f52ba3c62a9bedbb
josh-gelfand <113037258+josh-gelfand@users.noreply.github.com>
2022-10-24 07:58:15 -0400

    Add files via upload

---
+++ homework.java
@@ -0,0 +1,13 @@
+public class homework
+{
+    public static void main(String[] args)
+    {
+
+        for (int n=0; n<= 100; n+=3)
+        {
+            int k = 100 - n;
+            System.out.print(k + ",");
+        }
+
+    }
+}
